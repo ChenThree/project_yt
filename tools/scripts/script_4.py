e48ab06963de3471328f3b71a6bcb613304bf295
ChenThree <chenthree2@gmail.com>
2021-01-01 17:39:21 +0800

    [update] format

--- tools/scripts/script_4.py
+++ tools/scripts/script_4.py
@@ -1,8 +1,10 @@
 from ..utils.plot_helpers import plot_line_chart
 
+
 def script_4(project):
     # print action count
     print('action_count ==', project.get_action_count())
     # bar chart by month
     action_counts = project.get_action_counts_by_month()
-    plot_line_chart(action_counts, title='line chart for action counts by month')
+    plot_line_chart(action_counts,
+                    title='line chart for action counts by month')
--- tools/scripts/script_7.py
+++ tools/scripts/script_7.py
@@ -14,4 +14,5 @@ def script_7(project):
         writer.writerow(['action type', 'user_count'])
         for action, user_count in action_user_counts.items():
             writer.writerow([action, user_count])
-    print('Successfully output result to file: ./results/action_user_counts.csv')
+    print(
+        'Successfully output result to file: ./results/action_user_counts.csv')
